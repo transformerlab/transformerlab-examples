0e468cb7203ceacd951f86a3c2df96d6a73a1dbf
Mina Parham <mina.parham766@gmail.com>
2026-01-12 12:43:38 -0500

    Fix load dataset

--- basic-evals-metrics/train.py
+++ basic-evals-metrics/train.py
@@ -277,7 +277,7 @@ def run_evaluation():
         try:
             from datasets import load_dataset
 
-            dataset = load_dataset(dataset_name)
+            dataset = load_dataset(dataset_name, split="train")
             df = dataset["train"].to_pandas()
             lab.log("Dataset loaded successfully")
         except Exception as e:

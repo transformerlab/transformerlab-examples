92c3ce65b20bb0516df61354ee634b61bae361fb
Mina Parham <mina.parham766@gmail.com>
2026-03-20 12:09:05 -0400

    Fix gradio issue

--- inference-mlx-audio-tts/gradio_tts.py
+++ inference-mlx-audio-tts/gradio_tts.py
@@ -144,4 +144,4 @@ with gr.Blocks(title="MLX Audio TTS") as demo:
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, theme=gr.themes.Soft())
+    demo.launch(server_name="0.0.0.0", server_port=7860)
